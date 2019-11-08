f65ac005ad80eb951ee9290b61738d42ca03453c
Tamas K. Papp <tkpapp@gmail.com>
2019-11-08 09:45:02 +0100

    bump documenter versions
    Also refresh template a bit: test for CI environment, uncomment
    optional (but useful) keyword arguments so that the user can enable
    them on demand but they don't interfere if not needed.

--- templates/default/docs/make.jl
+++ templates/default/docs/make.jl
@@ -2,10 +2,13 @@ using Documenter, {PKGNAME}
 
 makedocs(
     modules = [{PKGNAME}],
-    format = Documenter.HTML(),
-    checkdocs = :exports,
+    format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
+    authors = "{USERNAME}",
     sitename = "{PKGNAME}.jl",
     pages = Any["index.md"]
+    # strict = true,
+    # clean = true,
+    # checkdocs = :exports,
 )
 
 deploydocs(
