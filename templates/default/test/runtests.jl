6d89e6302fe0ac95d198ac5a4d440d36b02a224c
Tamas K. Papp <tkpapp@gmail.com>
2023-05-25 11:28:41 +0200

    various minor updates (#43)
    * move default template CI Julia version to 1.6
    
    * add a call to JET
    
    * uncomment whole documentation template.
    
    reason: it should only be added to the repo when used. the commented
    version errors.
    
    * bump version

--- templates/default/test/runtests.jl
+++ templates/default/test/runtests.jl
@@ -2,3 +2,9 @@ using {PKGNAME}
 using Test
 
 # write tests here
+
+## NOTE add JET to the test environment, then uncomment
+# using JET
+# @testset "static analysis with JET.jl" begin
+#     @test isempty(JET.get_reports(report_package({PKGNAME}, target_modules=({PKGNAME},))))
+# end
