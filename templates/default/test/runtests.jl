fb5f010e1ad1dc2bd15cba35eb075993effa8031
Tamás K. Papp <tkpapp@gmail.com>
2023-06-08 17:28:27 +0200

    Add commented out tests w/ Aqua

--- templates/default/test/runtests.jl
+++ templates/default/test/runtests.jl
@@ -8,3 +8,11 @@ using Test
 # @testset "static analysis with JET.jl" begin
 #     @test isempty(JET.get_reports(report_package({PKGNAME}, target_modules=({PKGNAME},))))
 # end
+
+## NOTE add Aqua to the test environment, then uncomment
+# @testset "QA with Aqua" begin
+#     import Aqua
+#     Aqua.test_all({PKGNAME}; ambiguities = false)
+#     # testing separately, cf https://github.com/JuliaTesting/Aqua.jl/issues/77
+#     Aqua.test_ambiguities({PKGNAME})
+# end
