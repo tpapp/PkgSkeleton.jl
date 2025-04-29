18bffca5b4be858f9176368799903ae60cfb685e
Tamás K. Papp <tkpapp@gmail.com>
2025-04-29 12:48:58 +0200

    minor template update

--- templates/default/test/runtests.jl
+++ templates/default/test/runtests.jl
@@ -6,7 +6,7 @@ using Test
 ## NOTE add JET to the test environment, then uncomment
 # using JET
 # @testset "static analysis with JET.jl" begin
-#     @test isempty(JET.get_reports(report_package({PKGNAME}, target_modules=({PKGNAME},))))
+#     @test JET.test_package({PKGNAME}, target_modules=({PKGNAME},))
 # end
 
 ## NOTE add Aqua to the test environment, then uncomment
